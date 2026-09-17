7f361382fba8ad15a4b2bfa0775685fa423d0e6f
ayrton14-AEC <ayrton.bhat@gmail.com>
2026-09-17 02:46:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'console-v1';
+var CACHE = 'console-v3';
 var ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
